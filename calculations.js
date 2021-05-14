6baa93fce76a21007a564f48b9391f74ef1acafd
pcbugworld <84206005+pcbugworld@users.noreply.github.com>
2021-05-14 13:44:44 -0700

    initial comit

---
+++ calculations.js
@@ -0,0 +1,33 @@
+const COLOR_VALUES = {"red": 1, "yellow": 2, "blue": 3, "green" : 4, "black": 5, "white": 6, "purple": 3.14, "orange": 2.71, "none": 1.049 }
+var form = document.getElementById("calculator");
+form.addEventListener('submit', displaySwag);
+
+function displaySwag(event) {
+    event.preventDefault();
+    const nameVal =  document.getElementById("name").value;
+    const clothingVal =  document.getElementById("clothingColor").value;
+    const hairVal =  document.getElementById("hairDye").value;
+    const handednessRadios = document.getElementsByName('handedness');
+
+    let handednessVal;
+    for (var i = 0, length = handednessRadios.length; i < length; i++) {
+        if (handednessRadios[i].checked) {
+            handednessVal = handednessRadios[i].value;
+        }
+    }
+    const accessoryVal =  document.getElementById("accessoryCount").value;
+    const swagScore = calculateSwag(nameVal, clothingVal, hairVal, handednessVal, accessoryVal);
+    const resultDisplay = document.querySelector('#results');
+    resultDisplay.textContent = `Your swag score is ${Number.parseFloat(swagScore).toFixed(2)}`; 
+    return false;    
+}
+
+function calculateSwag(name, clothingColor, hairDye, handedness, accessoryCount) {
+    console.log(name, clothingColor, hairDye, handedness, accessoryCount);
+    nameLength = name.length;
+    clothingScore = COLOR_VALUES[clothingColor];
+    hairScore = COLOR_VALUES[hairDye];
+    handValue = handedness === 'right' ? 6 : 7;
+    console.log(nameLength, clothingScore, hairScore, handValue);
+    return (((clothingScore / handValue) ^ (accessoryCount + 1)) / (hairScore + nameLength));    
+}
